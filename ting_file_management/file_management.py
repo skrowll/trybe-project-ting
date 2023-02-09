da0cd7716f000c8c9e915abf335fcd2db62b3e6a
skrowll <lucasdejesus.rocha@hotmail.com>
2023-02-09 00:09:33 -0300

    fix: error flake8

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -6,9 +6,8 @@ def txt_importer(path_file):
         print("Formato inválido", file=sys.stderr)
     else:
         try:
-          with open(path_file, "r") as file:
-            return file.read().split("\n")
+            with open(path_file, "r") as file:
+                return file.read().split("\n")
 
         except FileNotFoundError:
             print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
-        
