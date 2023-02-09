232ebf40702d24e48c38983917459b35c679c3a2
skrowll <lucasdejesus.rocha@hotmail.com>
2023-02-09 00:02:27 -0300

    feat(Req. 02): implements function txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if '.txt' not in path_file:
+        print("Formato inválido", file=sys.stderr)
+    else:
+        try:
+          with open(path_file, "r") as file:
+            return file.read().split("\n")
+
+        except FileNotFoundError:
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        
