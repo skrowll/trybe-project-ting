78e6542c55d40fb1b47b2b43ab9766b1eca49f6f
skrowll <lucasdejesus.rocha@hotmail.com>
2023-02-09 00:45:15 -0300

    feat(Req. 03): implements function process on file_process (1/3)

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    files_names_list = []
+    for element in instance.queue:
+        files_names_list.append(element["nome_do_arquivo"])
+
+    if path_file not in files_names_list:
+        text_list = txt_importer(path_file)
+        result_dict = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(text_list),
+            "linhas_do_arquivo": text_list
+        }
+        instance.enqueue(result_dict)
+        print(result_dict, file=sys.stdout)
 
 
 def remove(instance):
