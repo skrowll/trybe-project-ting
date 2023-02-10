7bb7c746b4aa087556b6b17987ad5e819571ac8e
skrowll <lucasdejesus.rocha@hotmail.com>
2023-02-10 00:11:30 -0300

    feat(Req. 08): implements function search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -16,4 +16,20 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    queued = instance.queue
+    occurrences = []
+
+    for element in queued:
+        for index in range(len(element["linhas_do_arquivo"])):
+            if word.lower() in element["linhas_do_arquivo"][index].lower():
+                occurrences.append({
+                    "linha": index + 1,
+                    "conteudo": element["linhas_do_arquivo"][index]
+                    })
+        if len(occurrences) > 0:
+            return [{
+                "palavra": word,
+                "arquivo": element["nome_do_arquivo"],
+                "ocorrencias": occurrences
+                }]
+        return []
